ae280ce819585eb9d9991673de0dabfc299fb0d9
Vinicius De Oliveira Heckert <151755376+Vinih12@users.noreply.github.com>
2025-03-23 08:48:45 -0300

    Update Jogo.js
    Esqueci de Mostrar o resultado final, ent acabei de colocar ele

--- Jogo.js
+++ Jogo.js
@@ -21,3 +21,5 @@ while (chute != numeroSecreto) {
     }
 }
 
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
